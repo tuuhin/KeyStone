ef043da7874cc3f9ec5ba96f57862d646ac27fcb
tuuhin <tuhinbhowmick2513@gmail.com>
2025-11-12 15:56:20 +0530

    Included annotation to provide better docs Included swagger annotations for better docs for the controllers It maybe changes later in future

--- src/main/kotlin/com/sam/keystone/controllers/AuthController.kt
+++ src/main/kotlin/com/sam/keystone/controllers/AuthController.kt
@@ -1,13 +1,22 @@
 package com.sam.keystone.controllers
 
 import com.sam.keystone.dto.request.LoginUserRequest
+import com.sam.keystone.dto.request.RefreshTokenRequest
 import com.sam.keystone.dto.request.RegisterUserRequest
+import com.sam.keystone.dto.response.ErrorResponseDto
 import com.sam.keystone.dto.response.TokenResponseDto
 import com.sam.keystone.dto.response.UserResponseDto
 import com.sam.keystone.entity.User
+import com.sam.keystone.exceptions.UserAuthException
 import com.sam.keystone.mappers.toReposeDTO
 import com.sam.keystone.services.UsersService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
@@ -16,39 +25,166 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/auth")
-class AuthController(private val service: UsersService) {
+@Tag(
+    name = "User Authentication",
+    description = "User management routes"
+)
+class AuthController(
+    private val service: UsersService,
+) {
 
     @PostMapping("/register")
+    @Operation(summary = "Creates a new user from the given credentials and prepares a new token pair")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "New token pair created successfully",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(TokenResponseDto::class)),
+                ]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Unauthenticated user",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponseDto::class)),
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Invalid user credentials",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponseDto::class)),
+                ]
+            ),
+        ]
+    )
     fun registerUser(@RequestBody request: RegisterUserRequest): ResponseEntity<TokenResponseDto> {
-        return try {
-            val newUser = service.createNewUser(request)
-            ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(newUser)
-        } catch (_: IllegalArgumentException) {
-            ResponseEntity.badRequest().build()
-        }
+        val newUser = service.createNewUser(request)
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(newUser)
     }
 
     @PostMapping("/login")
+    @Operation(summary = "Checks the given credentials and prepares a new token pair")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "New token pair created successfully",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(TokenResponseDto::class)),
+                ]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Unauthenticated user",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponseDto::class)),
+                ]
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Invalid User",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponseDto::class)),
+                ]
+            ),
+        ]
+    )
     fun loginUser(@RequestBody request: LoginUserRequest): ResponseEntity<TokenResponseDto> {
-        return try {
-            val newUser = service.loginUser(request)
-            ResponseEntity
-                .status(HttpStatus.OK)
-                .body(newUser)
-        } catch (_: IllegalArgumentException) {
-            ResponseEntity.badRequest().build()
-        }
+        val newUser = service.loginUser(request)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(newUser)
     }
 
+    @GetMapping(name = "current_user")
     @SecurityRequirement(name = "Authorization")
-    @GetMapping
+    @Operation(summary = "Fetches the current authenticated user")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Authenticated User",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(UserResponseDto::class)),
+                ]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Unauthenticated user",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponseDto::class)),
+                ]
+            ),
+        ]
+    )
     fun getCurrentUser(auth: Authentication): ResponseEntity<UserResponseDto> {
         val user = auth.principal as? User
-        val response = user?.toReposeDTO() ?: return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-            .build()
-
+        val response = user?.toReposeDTO() ?: throw UserAuthException("No user found")
         return ResponseEntity.status(HttpStatus.OK).body(response)
     }
+
+
+    @PostMapping("/refresh")
+    @SecurityRequirement(name = "Authorization")
+    @Operation(summary = "Prepares a new token pair for the given refresh token")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "New token pair created successfully",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(TokenResponseDto::class)),
+                ]
+            ),
+            ApiResponse(
+                responseCode = "403",
+                description = "Unauthenticated user",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponseDto::class)),
+                ]
+            ),
+        ]
+    )
+    fun refreshToken(
+        @RequestBody request: RefreshTokenRequest,
+        auth: Authentication,
+    ): ResponseEntity<TokenResponseDto> {
+        // no authenticated used
+        val user = auth.principal as? User ?: throw UserAuthException("No authenticated user")
+
+        val newUser = service.generateNewTokenPairs(request, user)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(newUser)
+    }
+
+
+    @PostMapping("/logout")
+    @SecurityRequirement(name = "Authorization")
+    @Operation(summary = "Log out a given user")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "204", description = "Token blacklisted successfully"),
+            ApiResponse(
+                responseCode = "403",
+                description = "Unauthenticated user",
+                content = [
+                    Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponseDto::class)),
+                ]
+            ),
+        ]
+    )
+    fun logoutUser(@RequestBody request: RefreshTokenRequest, auth: Authentication): ResponseEntity<Any> {
+        // no authenticated used
+        if (auth.principal !is User) throw UserAuthException("No authenticated user")
+
+        service.blackListToken(request)
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+    }
+
 }
--- src/main/kotlin/com/sam/keystone/controllers/HealthController.kt
+++ src/main/kotlin/com/sam/keystone/controllers/HealthController.kt
@@ -1,5 +1,7 @@
 package com.sam.keystone.controllers
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/health")
+@Tag(name = "Health")
 class HealthController {
 
 
     @GetMapping
+    @Operation(summary = "A indication that the server is working")
     fun healthStatus() = mapOf("status" to "Ok")
 }
