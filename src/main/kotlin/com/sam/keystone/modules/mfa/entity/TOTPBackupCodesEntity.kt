5fe41944a0f0467be9bf256b270e12aca86249d4
tuuhin <tuhinbhowmick2513@gmail.com>
2025-12-12 03:02:12 +0530

    Included entity for backup code and totp TOTPEntity.kt and TOTPBackupCodesEntity.kt for storing the 2fa info and V9__include_new_table_totp.sql migration added

---
+++ src/main/kotlin/com/sam/keystone/modules/mfa/entity/TOTPBackupCodesEntity.kt
@@ -0,0 +1,27 @@
+package com.sam.keystone.modules.mfa.entity
+
+import jakarta.persistence.*
+import java.time.Instant
+
+@Entity
+@Table(name = "totp_backup_codes_table")
+class TOTPBackupCodesEntity(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "totp_backups_id", columnDefinition = "INTEGER")
+    var id: Long = 0L,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "totp_id", nullable = false, unique = false)
+    val totp: TOTPEntity? = null,
+
+    @Column(name = "is_used", nullable = false)
+    val isUsed: Boolean = false,
+
+    @Column(name = "totp_backup_code_hash", nullable = false)
+    val backUpCode: String,
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: Instant = Instant.now(),
+)
---
+++ src/main/kotlin/com/sam/keystone/modules/mfa/entity/TOTPEntity.kt
@@ -0,0 +1,51 @@
+package com.sam.keystone.modules.mfa.entity
+
+import com.sam.keystone.modules.user.entity.User
+import jakarta.persistence.*
+import java.time.Instant
+
+@Entity
+@Table(name = "totp_table")
+class TOTPEntity(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "totp_id", columnDefinition = "INTEGER")
+    var id: Long = 0L,
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    val user: User? = null,
+
+    @Column(name = "is_totp_enabled", nullable = false)
+    var isEnabled: Boolean = false,
+
+    @Column(name = "totp_secret_encrypted", nullable = false)
+    var totpSecret: String = "",
+
+    @OneToMany(
+        fetch = FetchType.LAZY,
+        mappedBy = "totp",
+        cascade = [CascadeType.ALL],
+        orphanRemoval = true
+    )
+    var backupCodes: MutableSet<TOTPBackupCodesEntity> = mutableSetOf(),
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: Instant = Instant.now(),
+
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: Instant = Instant.now(),
+) {
+
+    @PrePersist
+    fun onCreate() {
+        createdAt = Instant.now()
+        updatedAt = createdAt
+    }
+
+    @PreUpdate
+    fun onUpdate() {
+        updatedAt = Instant.now()
+    }
+}
