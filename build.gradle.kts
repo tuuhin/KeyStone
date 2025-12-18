dbd400ebfc2b475105e62d74bb71a163ba55aaa8
tuuhin <tuhinbhowmick2513@gmail.com>
2025-12-18 23:20:01 +0530

    Updated kotlin version and README.md No more experimental time api annotations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.spring") version "2.3.0"
     id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.2.21"
+    kotlin("plugin.jpa") version "2.3.0"
 }
 
 group = "com.sam"
